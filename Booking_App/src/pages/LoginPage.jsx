8851a3dd542e0bf3e7d2eaeca3d1ed3b65a2ef78
ManvendraPSdev <welcome2github@gmail.com>
2024-01-06 13:55:39 +0530

    Header Added

---
+++ Booking_App/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+export default function(){
+    return (
+        <div className="mt-4">
+            <h1 className="text-4xl text-center ">Login</h1>
+            <form className="max-w-md mx-auto " >
+                <input type="email" placeholder="usseremail.com" />
+                <input type="Password " placeholder="Password" />
+                <button>Login</button>
+            </form>
+        </div>
+    )
+}
---
+++ notes.md
@@ -0,0 +1,5 @@
+# api for the backend and Booking App is for the Front End 
+
+# For marking place we need to first login for that we use react router 
+# Install the react routes and Now we need to add those routes in the App.jsx
+- Adding another Route for Registering page
